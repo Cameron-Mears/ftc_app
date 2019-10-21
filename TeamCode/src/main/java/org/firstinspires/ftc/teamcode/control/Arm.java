d860a7c9f9e164ea7c3b1526b4390d6602707ff7
cameron <cameronmears.bc@gmail.com>
2019-10-20 20:48:30 -0700

    update/test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Arm.java
@@ -7,133 +7,90 @@ package org.firstinspires.ftc.teamcode.control;
  including any sub arms
 */
 
+import org.firstinspires.ftc.teamcode.physicscalculations.*;
+
 public class Arm
 {
-    private static final double FORCE_GRAVITY = 9.8;
+    private static final double FORCE_GRAVITY = 9.80;
 
-    private Arm subArm; //if there is another arm joined
+    public final Arm subArm; //if there is another arm joined
     private PID pid;
     private Motor motor;
-    private double distance_to_center_of_mass;
-    private double mass;
-    private double torque_angle_radians;
-    private double length;
-    private double target;
+    public final double distanceToCenterOfMass;
+    public final double mass; //total mass
+    public final double massOfBeam; //mass of the beam
+    public PointMass[] attachments;
+    public final double length;
 
-    public Arm(Motor motor, Arm subArm, Arm parentArm, double mass, double distance, double K_i, double K_d, double K_p)
+    public Arm(Motor motor, Arm subArm, double mass, double massOfBeam, double length, double distanceToCenterOfMass, double K_i, double K_d, double K_p)
     {
         pid = new PID(motor.encoder, 0, 0,0 );
         this.motor = motor;
-    }
+        this.subArm = subArm;
+        this.massOfBeam = massOfBeam;
+        this.length = length;
+        this.mass = mass;
+        this.distanceToCenterOfMass = distanceToCenterOfMass;
 
+    }
+    //x and y are relative to base of arm
+    public void initPointMasses(PointMass ...masses)
+    {
+        this.attachments = masses;
+    }
 
     public double calculate_torque_compensation()
     {
-        double sum = this.sum_torque(0 , 0, new double[1], new double[1]);
-        double[] point = this.getCenterOfMass(new double[1], new double[1], new double[1], new double[1], 0);
+        double sum = this.sum_torque(0 , 0, 0, 0);
+        PointMass point = this.getCenterOfMass(0, 0, 0, 0, 0);
         double compensationPower = (sum * this.motor.free_rpm * Motor.NEWTON_METER_POWER_CONSERVION)/this.motor.max_power;
         return compensationPower;
     };
 
     //make sure when calling this method sum is zero
-    public double sum_torque(double sum, int depth, double[] angles, double[] distances)
+    public double sum_torque(double sum, double angles, int depth, double distances)
     {
-        depth++;
-        double armAngle = this.getAngle();
-        if (angles.length < depth) angles = doubleArrayExpand(angles, angles.length * 2);
-        if (distances.length < depth) angles = doubleArrayExpand(angles, angles.length * 2);
-        angles[depth - 1] = armAngle;
+        depth ++;
+        double armAngle = this.computeAngle();
+        angles += armAngle;
 
         double calculationAngle = 0;
         double totalDistance = 0;
-        for (double distance: distances)
-        {
-            totalDistance += distance;
-        }
 
-        for (double angle : angles)
-        {
-            calculationAngle += angle;
-        }
 
         if (depth % 2 != 0) calculationAngle = 180 - calculationAngle; //odd call 180 - angle
 
         calculationAngle = Math.toRadians(90 - calculationAngle);
-        distances[depth - 1] = Math.cos(90 + calculationAngle);
-        sum += Math.sin(calculationAngle) * (this.distance_to_center_of_mass + totalDistance) * FORCE_GRAVITY; //calculate moment on arm
-        if (this.subArm != null) sum += this.subArm.sum_torque(sum, depth, angles, distances); //sum the torque acting on subjoints
+        distances += Math.cos(90 + calculationAngle);
+        sum += Math.sin(calculationAngle) * (this.distanceToCenterOfMass + totalDistance) * FORCE_GRAVITY; //calculate moment on arm
+        if (this.subArm != null) sum += this.subArm.sum_torque(sum, angles, depth, distances); //sum the torque acting on subjoints
         return sum;
     }
     /*
-       returns an array of size 2 , contaning 2 points, first the x point, then y point
-       of the center of mass of the form the given arm, in the unit the lengths were measured in
+       returns an array of size 2 , contaning 3 numbers, first the x point, then y point
+       of the center of mass of the form the given arm, and third the total mass
 
     */
 
-    private double[] getCenterOfMass(double[] xVecs, double[] xVecsMass, double[] yVecs, double[] yVecsMass, int depth)
+    public PointMass getCenterOfMass(double xTotalLength, double yTotalLength, double xVecsMass, double yVecsMass, double masses)
     {
-        depth++;
-        double angle = Math.toRadians(this.getAngle());
-        double xVec = Math.cos(angle) * this.distance_to_center_of_mass;
-        double yVec = Math.sin(angle) * this.distance_to_center_of_mass;
-        if (xVecs.length < depth) xVecs = doubleArrayExpand(xVecs, xVecs.length * 2);
-        if (xVecsMass.length < depth) xVecsMass = doubleArrayExpand(xVecsMass, xVecsMass.length * 2);
-        if (yVecsMass.length < depth) yVecsMass = doubleArrayExpand(yVecsMass, yVecsMass.length * 2);
-        if (yVecs.length <= depth) yVecs = doubleArrayExpand(yVecs, yVecs.length * 2);
-        xVecs[depth - 1] = xVec;
-        xVecsMass[depth - 1] = xVec * this.mass;
-        yVecs[depth - 1] = yVec;
-        yVecsMass[depth - 1] = yVec * this.mass;
-        if (this.subArm == null)
-        {
-            double numerator = 0;
-            double denomanator = 0;
-            for (double xVecMass: xVecsMass)
-            {
-                numerator += xVecMass;
-            }
+        double angle = Math.toRadians(this.computeAngle());
 
-            for (double xvec: xVecs)
-            {
-                denomanator += xvec;
-            }
+        double cos = Math.cos(angle);
+        double sin = Math.sin(angle);
 
-            double xCenter = numerator/denomanator;
-            numerator = denomanator = 0;
+        xVecsMass += ((cos * this.distanceToCenterOfMass) + xTotalLength) * this.mass;
+        yVecsMass += ((sin * this.distanceToCenterOfMass) + yTotalLength) * this.mass;
 
-            for (double yVecMass: yVecsMass)
-            {
-                numerator += yVecMass;
-            }
 
-            for (double yvec: yVecs)
-            {
-                denomanator += yvec;
-            }
-
-            double yCenter = numerator/denomanator;
-
-            double[] point = new double[2];
-            point[0] = xCenter;
-            point[1] = yCenter;
-            return point;
-        }
-        else return subArm.getCenterOfMass(xVecs, xVecsMass, yVecs, yVecsMass, depth);
+        xTotalLength += cos * this.length;
+        yTotalLength += sin * this.length;
+        masses += this.mass;
+        if (this.subArm == null) return new PointMass(xVecsMass/xTotalLength, yVecsMass/yTotalLength, masses);
+        else return subArm.getCenterOfMass(xTotalLength, yTotalLength, xVecsMass, yVecsMass, masses);
     };
 
-    private double[] doubleArrayExpand(double[] array, int newSize)
-    {
-        if (array.length - 1 >= newSize) return null;
-        double[] newArray = new double[newSize];
-
-        for (int index = 0; index < array.length - 1; index++)
-        {
-            newArray[index] = array[index];
-        }
-        return newArray;
-    }
-
-    public double getAngle()
+    public double computeAngle()
     {
         this.motor.encoder.update();
         double encoderPosition = this.motor.encoder.getPosition();
